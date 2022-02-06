8dd87cda145b20a6fea1cd5348eee3c252bd63e4
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2022-02-06 18:33:26 +0300

    quick Pizza id customization

--- src/gql-client/apollo-client.config.js
+++ src/gql-client/apollo-client.config.js
@@ -1,6 +1,14 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Pizza: {
+      keyFields: ['id', 'name'],
+    },
+  },
+});
+
 export default new ApolloClient({
   uri: 'http://localhost:3001/graphql',
-  cache: new InMemoryCache(),
+  cache,
 });
